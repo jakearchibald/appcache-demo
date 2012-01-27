403355c96997ec2dd163b17d8c5bfdd0c35ee27e
Jake Archibald <jaffathecake@gmail.com>
2012-01-27 18:18:25 +0000

    Fixing urls, adding basic css

--- offline-iframe/js/offliner.js
+++ offline-iframe/js/offliner.js
@@ -2,7 +2,7 @@ var offliner = (function() {
 	return {
 		addCachingIframe: function() {
 			$( document.body ).prepend(
-				'<iframe src="/offline-ala/offline.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
+				'<iframe src="/offline-iframe/offline.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
 			);
 		}
 	};
--- simple-fallback/js/offliner.js
+++ simple-fallback/js/offliner.js
@@ -2,7 +2,7 @@ var offliner = (function() {
 	return {
 		addCachingIframe: function() {
 			$( document.body ).prepend(
-				'<iframe src="/offline-ala/fallback.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
+				'<iframe src="/simple-fallback/fallback.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
 			);
 		}
 	};
