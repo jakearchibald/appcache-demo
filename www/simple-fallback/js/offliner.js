6823b934b619d2cbfbd4a093b38caf0ca5ce9048
Jake Archibald <jaffathecake@gmail.com>
2012-04-15 16:54:13 +0100

    Changing relative urls

--- www/simple-fallback/js/offliner.js
+++ www/simple-fallback/js/offliner.js
@@ -2,7 +2,7 @@ var offliner = (function() {
 	return {
 		addCachingIframe: function() {
 			$( document.body ).prepend(
-				'<iframe src="/offline-demos/www/simple-fallback/fallback.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
+				'<iframe src="/2012appcachedouche/offline-demos/www/simple-fallback/fallback.html" style="position:absolute;top:-999em;visibility:hidden"></iframe>'
 			);
 		}
 	};
